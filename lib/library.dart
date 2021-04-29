2a5279c32b1f61c78caf46036d56d703cd19386f
seashell-scientist <50582060+seashell-scientist@users.noreply.github.com>
2021-04-29 15:09:07 -0500

    Update library.dart

--- lib/library.dart
+++ lib/library.dart
@@ -21,21 +21,93 @@ class LibraryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(appBar: HootAppBar(), body: Stack(children: [
-        Container(decoration:CustomBackground().baseBackgroundDecoration()),
-        Column(crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BookCover(coverURL: sampleBook.coverUrl,)
-        ],)
-      ],))
-    ); //Text('wrapped rows inside a single scrollable column');
+        home: Scaffold(
+            appBar: HootAppBar(),
+            body: Stack(
+              children: [
+                Container(
+                    decoration: CustomBackground().baseBackgroundDecoration()),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: ListView(
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: CategoryCard(categoryTitle: 'Book Category 1',),
+                              ),
+                            ),
+                            Wrap(spacing: 12, runSpacing: 12, children: [
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                            ]),
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: CategoryCard(categoryTitle: 'Book Category 2',),
+                              ),
+                            ),
+                            Wrap(spacing: 12, runSpacing: 12, children: [
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                            ]),
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: CategoryCard(categoryTitle: 'Book Category 3',),
+                              ),
+                            ),
+                            Wrap(spacing: 12, runSpacing: 12, children: [
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                            ]),
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: CategoryCard(categoryTitle: 'Book Category 4',),
+                              ),
+                            ),
+                            Wrap(spacing: 12, runSpacing: 12, children: [
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                              BookCover(coverURL: sampleBook.coverUrl),
+                            ]),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ))); //Text('wrapped rows inside a single scrollable column');
   }
 }
 
 class BookCover extends StatefulWidget {
   final String coverURL;
-  const BookCover({Key key, this.coverURL,})
-      : super(key: key);
+  const BookCover({
+    Key key,
+    this.coverURL,
+  }) : super(key: key);
   @override
   _BookCoverState createState() => _BookCoverState();
 }
@@ -43,18 +115,45 @@ class BookCover extends StatefulWidget {
 class _BookCoverState extends State<BookCover> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () {},
-    style: ElevatedButton.styleFrom(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25),
+    return SizedBox(
+      height: MediaQuery.of(context).size.width * .15,
+      width: MediaQuery.of(context).size.width * .15,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+          elevation: 10,
         ),
-        elevation: 10,
+        clipBehavior: Clip.antiAlias,
+        autofocus: false,
+        child: Image.network(widget.coverURL),
       ),
-      clipBehavior: Clip.none,
-      autofocus: false,
-      
-              child: Image.network(widget.coverURL),
-            
+    );
+  }
+}
+
+class CategoryCard extends StatefulWidget {
+  final String categoryTitle;
+  const CategoryCard({
+    Key key,
+    this.categoryTitle,
+  }) : super(key: key);
+  @override
+  _CategoryCardState createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .05,
+      child: Align( alignment: Alignment.bottomLeft,
+              child: Text(widget.categoryTitle,
+            textScaleFactor: 1.5),
+      ),
     );
   }
 }
