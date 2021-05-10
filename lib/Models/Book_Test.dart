b61fa41f83414e6382ffe0b9a1410d81b07868d9
seashell-scientist <50582060+seashell-scientist@users.noreply.github.com>
2021-05-10 11:49:32 -0500

    get firestore stuff
    importing book models from mobile to test firestore interactions

---
+++ lib/Models/Book_Test.dart
@@ -0,0 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import './Category.dart';
+
+class Book {
+  final String id;
+  final String title;
+  final String description;
+  final List<String> authors;
+  final List<String> illustrators;
+  final List<String> narrators;
+  final List<String> curators;
+  final String ageRange;
+  final String gradeLevel;
+  final String pdfUrl;
+  final String coverImageUrl;
+  final String jsonUrl;
+  final String categoryIdentifier;
+  final List<String> tags;
+  final bool isFree;
+
+  Book({
+    this.id,
+    this.title,
+    this.description,
+    this.authors,
+    this.illustrators,
+    this.narrators,
+    this.curators,
+    this.ageRange,
+    this.gradeLevel,
+    this.pdfUrl,
+    this.coverImageUrl,
+    this.jsonUrl,
+    this.categoryIdentifier,
+    this.tags,
+    this.isFree,
+  });
+
+  factory Book.fromFirestore(DocumentSnapshot doc) {
+    Map data = doc.data(); //!;
+    return Book(
+      id: doc.id,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      authors: data['authors']?.cast<String>() ?? [],
+      illustrators: data['illustrators']?.cast<String>() ?? [],
+      narrators: data['narrators']?.cast<String>() ?? [],
+      curators: data['curators']?.cast<String>() ?? [],
+      ageRange: data['ageRange'] ?? '',
+      gradeLevel: data['gradeLevel'] ?? '',
+      pdfUrl: data['pdfUrl'] ?? '',
+      coverImageUrl: data['coverImageUrl'] ?? '',
+      jsonUrl: data['jsonUrl'] ?? '',
+      categoryIdentifier: data['categoryIdentifier'] ?? '',
+      tags: data['tags']?.cast<String>() ?? [],
+      isFree: data['isFree'] ?? false,
+    );
+  }
+
+  get summary => null;
+
+  bool isCategory(Category category) {
+    return category.identifier == categoryIdentifier;
+  }
+
+  @override
+  String toString() {
+    return "id: $id, title: $title, categoryIdentifier: $categoryIdentifier";
+  }
+}
---
+++ lib/Models/Category.dart
@@ -0,0 +1,6 @@
+class Category {
+  final String identifier; //dont'forget to put back the String? for main null safety
+  final String label;
+
+  const Category({this.identifier, this.label});
+}
